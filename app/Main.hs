50398ff93e84b386c009f158f8b4bfaed2a42b71
pacokwon <haechank@gmail.com>
2023-07-02 22:27:58 +0900

    Support interactive mode after file execution

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,9 @@ import System.IO (hFlush, stdout)
 import Eval (evalSexpr, Env, globalEnvironment)
 import qualified Data.Text.IO as TIO
 import qualified Data.Map as Map
-import Control.Monad.State (runStateT, evalStateT)
+import Control.Monad.State (runStateT)
+import Control.Monad (void)
+import Data.Foldable (forM_)
 
 repl :: IO ()
 repl = repl' globalEnvironment
@@ -25,22 +27,33 @@ repl' env = do
             Left err -> putStrLn ("Error: " ++ err) >> repl' env
             Right (result, env') -> print result >> repl' env'
 
-runFile :: String -> IO ()
+runFile :: String -> IO (Maybe Env)
 runFile filename = do
     input <- TIO.readFile filename
-    case flip evalStateT globalEnvironment . mapM evalSexpr . parseProgram $ input of
-        Left err -> putStrLn $ "Error: " ++ err
-        Right result -> mapM_ print result
+    case flip runStateT globalEnvironment . mapM evalSexpr . parseProgram $ input of
+        Left err -> do
+            putStrLn $ "Error: " ++ err
+            return Nothing
+        Right (values, env) -> do
+            mapM_ print values
+            return . Just $ env
 
 main :: IO ()
 main = do
     args <- getArgs
-    if null args then
-        repl
-    else do
-        let filename = head args
-        exists <- doesFileExist filename
-        if exists then
-            runFile filename
-        else
-            putStrLn $ "File named \"" ++ filename ++ "\" does not exist!"
+    case args of
+        [] -> repl
+        [filename] -> do
+            exists <- doesFileExist filename
+            if exists then void (runFile filename)
+            else
+                putStrLn $ "File named \"" ++ filename ++ "\" does not exist!"
+        ["-i", filename] -> do
+            exists <- doesFileExist filename
+            if exists then do
+                menv <- runFile filename
+                forM_ menv repl'
+            else
+                putStrLn $ "File named \"" ++ filename ++ "\" does not exist!"
+        [_, _] -> putStrLn "Invalid Flag. The only available flag is -i!"
+        _ -> putStrLn "Too many arguments!"
